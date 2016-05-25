5948925a21578bcad0271d44954c84451e7705bf
Jordi Urmeneta <jordi.urmeneta@gmail.com>
2016-05-25 17:59:30 +0200

    define online tile source base, for offline map

--- app/src/main/java/com/example/android/osmdroidofflinedemo/MainActivity.java
+++ app/src/main/java/com/example/android/osmdroidofflinedemo/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.android.osmdroidofflinedemo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class MainActivity extends Activity {
 
     public static final GeoPoint BERLIN = new GeoPoint(52.516667, 13.383333);
+
+    public static final OnlineTileSourceBase LOCAL_OSM_FILES = new XYTileSource("MapquestOSM",
+            ResourceProxy.string.mapquest_osm, 0, 15, 256, ".png", new String[] { });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,7 @@ public class MainActivity extends Activity {
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
         mapView.setUseDataConnection(true);
-        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        mapView.setTileSource(LOCAL_OSM_FILES);
 
         IMapController mapViewController = mapView.getController();
         mapViewController.setZoom(15);
